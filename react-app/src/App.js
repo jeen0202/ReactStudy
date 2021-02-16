c222a475bdea7f3ee5e82600cb02a9f6935f4ca3
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-16 18:22:14 +0900

    React의 기본 구조 파악

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import React, {Component} from 'react';
+class App extends Component {
+  render(){
+    return (
+    <div className="App">     
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      Hello React!!
     </div>
-  );
+    );
+  }
 }
-
 export default App;
