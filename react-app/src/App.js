5468c3f8d190218745a829d6c0ef85c790e97f58
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-17 19:01:27 +0900

    Component를 파일로 분리하여 사용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,43 +1,13 @@
 import './App.css';
 import React, {Component} from 'react';
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-class TOC extends Component{
-  render(){
-    return(
-      <ul>            
-        <li><a href ="1.html">HTML</a></li>
-        <li><a href ="2.html">CSS</a></li>
-        <li><a href ="2.html">JavaScrpit</a></li>
-      </ul>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-      <h2>{this.props.title}</h2>
-      {this.props.desc}
-  </article> 
-    );
-  }
-}
 class App extends Component {
   render(){
     return (
-    <div className="App">
-      <Subject title = "WEB" sub="World wide web"></Subject>
+    <div className="App">      
       <Subject title = "React" sub="For UI"></Subject>  
       <TOC></TOC>
       <Content title = "HTML" desc = "HTML is HyperText Markup Language."></Content>
---
+++ react-app/src/components/TOC.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TOC extends Component{
+    render(){
+      return(
+        <ul>            
+          <li><a href ="1.html">HTML</a></li>
+          <li><a href ="2.html">CSS</a></li>
+          <li><a href ="2.html">JavaScrpit</a></li>
+        </ul>
+      );
+    }
+  }
+export default TOC;
