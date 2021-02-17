4d1a8a10dc220cccf6ef2ec127d5b462dd441b11
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-17 18:39:30 +0900

    HTML=>Component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
+
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+class TOC extends Component{
+  render(){
+    return(
+      <ul>            
+        <li><a href ="1.html">HTML</a></li>
+        <li><a href ="2.html">CSS</a></li>
+        <li><a href ="2.html">JavaScrpit</a></li>
+      </ul>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+      <h2>HTML</h2>
+      HTML is HyperText Markup Language.
+  </article> 
+    );
+  }
+}
 class App extends Component {
   render(){
     return (
     <div className="App">
-      Hello, React!!
+      <Subject></Subject>
+      <TOC></TOC>
+      <Content></Content>
     </div>
     );
   }
