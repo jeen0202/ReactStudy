db0c8bf9b008ad87bd1eb897d3f6d3cbda8472d8
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-18 17:01:55 +0900

    bind와 setState를 사용하여 state 변경

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
       welcome:{title:"Welcome",desc:"Hello. React!!"},
       subject:{title:"WEB", sub:"World Wide Web"},
       contents:[
@@ -39,8 +39,12 @@ class App extends Component {
       <header>
           <h1><a href = "/" onClick={function(e){
             console.log(e);
-            e.preventDefault();           
-          }}>{this.state.subject.title}</a></h1>
+            e.preventDefault();
+            // this.state.mode ='welcome';
+            this.setState({
+              mode:'welcome'
+            })           
+          }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
         </header>
       <TOC data = {this.state.contents}></TOC>
