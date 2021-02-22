30bac86aafbd5a131d360f2a4c6d4a269e9ae971
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-22 18:24:32 +0900

    hook을 사용한 function style의 state사용

--- class-function/README.md
+++ class-function/README.md
@@ -6,3 +6,8 @@
 ### function
     스스로가 render 함수
     props를 매개변수로 받아 사용
+## Hook
+    기존버전과의 호환을 깨지않으면서 Class에서만 사용가능했던 기능(state등)을 사용가능하게 해주는 신규기능
+### Hook을 사용한 State 생성    
+    Hook기능을 통해 function style에서도 state를 사용할 수 있다.
+    React.useState() 를 사용하여 state객체를 생성하지 않고 state를 사용할 수 있다.         
--- class-function/src/App.js
+++ class-function/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 function App() {
   return (
     <div className="container">
@@ -10,22 +10,61 @@ function App() {
   );
 }
 function FuncComp(props){
+  //hook을 사용한 state 생성 => useState(초기값)
+  // 1번째 인자 : state 값,  2번째 인자 : setState 함수
+  // var numberState = useState(props.initNumber);  
+  // var number = numberState[0];
+  // var setNumber = numberState[1];
+  // var dateState = useState((new Date()).toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  //코드 간소화
+  var [number,setNumber] = useState(props.initNumber);
+  var [_date,setDate] = useState((new Date()).toString());
+  
   return (
     <div className="container">
         <h2>function style component</h2>
-        <p>Number : {props.initNumber}</p>
+        <p>Number : {number}</p>
+        <p>Date : {_date}</p>
+        <input type="button" value = "random" onClick={
+          ()=>{
+            //function style에서 state의 값을 변경하는 방법
+            //state객체의 2번째 인자의 매개변수로 변경하고자 하는 값 입력            
+            setNumber(Math.random())
+          }
+        }></input>
+        <input type="button" value = "date" onClick={
+          ()=>{          
+            setDate((new Date()).toString())
+          }
+        }></input>        
     </div>
   );
 }
 class ClassComp extends React.Component{
   state ={
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date : (new Date()).toString()
   }
   render(){
+    console.log("ClassComp Render")
     return (
       <div className="container">
         <h2>Class style component</h2>
-        <p>Number : {this.props.initNumber}</p>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input type="button" value = "random" onClick={
+          ()=>{
+            this.setState({number:Math.random()})
+          }
+        }></input>
+        <input type='button' value = "date" onClick={
+          ()=>{
+            this.setState({date:(new Date()).toString()})
+          }
+        }></input>
     </div>
     )
   }
