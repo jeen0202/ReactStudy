c7d9657080b0952dfb431d99d04cb935f18e3120
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-22 18:01:16 +0900

    props를 사용하는 방법

--- class-function/README.md
+++ class-function/README.md
@@ -1,4 +1,8 @@
 # React의 Class sytle 과 Function sytle
 ## Class vs Function
-class : render 함수를 통해 값을 반환
-function : 스스로가 render 함수
+### class
+    render 함수를 통해 값을 반환
+    props 사용시 this.props 사용
+### function
+    스스로가 render 함수
+    props를 매개변수로 받아 사용
--- class-function/src/App.js
+++ class-function/src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import React from 'react';
 function App() {
   return (
-    <div className="App">
-      Hello world
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+    <div className="container">
+      <h1>Hello world</h1>
+      <FuncComp initNumber = {2}></FuncComp>
+      <ClassComp initNumber = {2}></ClassComp>
     </div>
   );
 }
-function FuncComp(){
+function FuncComp(props){
   return (
     <div className="container">
         <h2>function style component</h2>
+        <p>Number : {props.initNumber}</p>
     </div>
   );
 }
 class ClassComp extends React.Component{
+  state ={
+    number:this.props.initNumber
+  }
   render(){
     return (
       <div className="container">
         <h2>Class style component</h2>
+        <p>Number : {this.props.initNumber}</p>
     </div>
     )
   }
