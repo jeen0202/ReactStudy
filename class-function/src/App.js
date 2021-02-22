4cb5ad5dbc7c97be54d8c257156b761d9cce6365
Sejin Kim <jeen0202@korea.ac.kr>
2021-02-22 17:56:32 +0900

    class style과 function style로 component 생성

--- class-function/src/App.js
+++ class-function/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
       Hello world
+      <FuncComp></FuncComp>
+      <ClassComp></ClassComp>
     </div>
   );
 }
-
+function FuncComp(){
+  return (
+    <div className="container">
+        <h2>function style component</h2>
+    </div>
+  );
+}
+class ClassComp extends React.Component{
+  render(){
+    return (
+      <div className="container">
+        <h2>Class style component</h2>
+    </div>
+    )
+  }
+}
 export default App;
