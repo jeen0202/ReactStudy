6052f89ad0ecd4852d1210ff9bfbc41dd035a10b
Sejing_Mac <jeen0202@korea.ac.kr>
2022-05-17 23:46:07 +0900

    05/17 React 환경설정 & 기초실행 복습

---
+++ review-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import TestComponet from './components/test';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <h1>Welcome to React</h1>
+        <TestComponet></TestComponet>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ review-app/src/components/test.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Test extends Component{
+    render(){
+        console.log('test rendering');
+        return(
+            <header>
+                <h1>This is Test title</h1>
+            </header>
+        )
+    }
+}
+export default Test;
