62bd637c613648ba05495cd910cbc6649d25effe
Sejing_Mac <jeen0202@korea.ac.kr>
2022-05-18 23:48:25 +0900

    05/18 함수형 component, hook Reviw

--- review-app/src/App.js
+++ review-app/src/App.js
@@ -1,15 +1,26 @@
-
+import React, {useState} from 'react';
+import TestComponent from './components/test';
 import './App.css';
-import TestComponet from './components/test';
+function useWorkday(){
+  const [isWorkday, setIsworkday] = useState(null);
+
+  if(new Date().getDay%6 >0)
+    setIsworkday(true)
+  return isWorkday
+}
 function App() {
+  let workday = useWorkday()?"근무일":"휴일";
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <body>
+      <div className='body'>
         <h1>Welcome to React</h1>
-        <TestComponet></TestComponet>
-      </body>
+        <TestComponent name="Function Componet Props" buttonstate='false'></TestComponent>
+        <TestComponent name="Function Props Change Test" buttonstate='true'></TestComponent>
+        {/* React에서의 변수 반영 */}
+        <p>오늘은 {workday}입니다</p> 
+      </div>
     </div>
   );
 }
--- review-app/src/components/test.jsx
+++ review-app/src/components/test.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from "react";
-
-class Test extends Component{
-    render(){
-        console.log('test rendering');
-        return(
-            <header>
-                <h1>This is Test title</h1>
-            </header>
-        )
-    }
+import React,{useEffect, useState} from 'react';
+const Test = (props)=>{
+    const [count,setCount] = useState(0);
+    
+    useEffect(()=>{
+        if (props.buttonstate ==='true') {
+            console.log(props.buttonstate);
+            let btn = document.getElementsByTagName('button');
+            btn[0].disabled = true;
+        }
+    })
+    return (
+        <div>
+            <h1>{props.name} Test입니다.</h1>
+            <p>당신은 버튼을 {count}번 클릭했습니다.</p>
+            <button onClick={()=> setCount(count + 1)}>
+                Click Here!
+            </button>
+        </div>
+    )
 }
 export default Test;
